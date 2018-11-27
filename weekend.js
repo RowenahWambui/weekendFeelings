00c86c024ac0f3e7f771be87ab999a9b2a6544d3
RowenahWambui <suzygracy@gmail.com>
2018-11-27 12:55:14 +0300

    first commit

---
+++ weekend.js
@@ -0,0 +1,12 @@
+var money = prompt("How much money do I have?");
+
+function todoList(){
+    if(money > 0){
+        window.alert("Buy wedding gift for crush");
+    }else{
+        //not going to display this to everyone
+        console.log("Double up on the happy pills",
+        " and don't cry Rowenah");
+    }
+}
+todoList();
